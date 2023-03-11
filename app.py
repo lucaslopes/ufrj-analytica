5ae7992c8e7a170faec8c07fb4910f8ba87cea8d
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2023-03-11 16:56:49 -0300

    Define JSON structure of the route /age

--- app.py
+++ app.py
@@ -1,15 +1,55 @@
+import datetime
+from dateutil.relativedelta import relativedelta
 from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
 
 
+def str2date(
+        date: str,
+    ) -> datetime.date:
+    return datetime.datetime.strptime(date, '%Y-%m-%d').date()
+
+
+def today(
+        output_str: bool = False,
+        format: str = '%Y-%m-%d',
+    ) -> datetime.date:
+    today_date = datetime.date.today()
+    if output_str:
+        today_date = today_date.strftime(format)
+    return today_date
+
+
+def calc_age(
+        birthdate: datetime.date,
+        date: datetime.date = None,
+    ) -> int:
+    if type(birthdate) is str:
+        birthdate = str2date(birthdate)
+    if type(date) is str:
+        date = str2date(date)
+    date = today() if date is None else date
+    age = relativedelta(date, birthdate).years
+    return age
+
+
 @app.route('/age', methods=['POST'])
 def age():
     data = request.json
     name = data['name']
-    quote = f'Hello, {name}!'
-    return jsonify({'quote': quote})
+    birthdate = str2date(data['birthdate'])
+    future_date = str2date(data['date'])
+    fut_date_str = future_date.strftime('%d/%m/%Y')
+    ageNow = calc_age(birthdate)
+    ageThen = calc_age(birthdate, future_date)
+    quote = f'Olá, {name}! Você tem {ageNow} anos e em {fut_date_str} você terá {ageThen} anos.'
+    res = jsonify({
+        'ageNow': ageNow,
+        'ageThen': ageThen,
+        'quote': quote})
+    return res
 
 
 def main():
--- test.py
+++ test.py
@@ -1,8 +1,15 @@
+import datetime
 import requests
 
 
 localhost = 'http://localhost:5000/'
-data = {'name': 'Lucas'}
+data = {
+    'name': 'Lucas Lopes',
+    'birthdate': '1995-07-12',
+    'date': '2050-07-12'}
+    # 'date': datetime.date.today().strftime('%Y-%m-%d')}
+
+
 req = requests.post(localhost+'age', json=data)
 res = req.json()
 print(res)
