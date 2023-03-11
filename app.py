3918ae2301d1ab91ed2314d65d6eb4cb97ed7193
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2023-03-11 13:23:56 -0300

    Create new route POST /age

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
+@app.route('/age', methods=['POST'])
+def age():
+    data = request.json
+    name = data['name']
+    quote = f'Hello, {name}!'
+    return jsonify({'quote': quote})
+
+
 def main():
     app.run(debug=True)
     return True
